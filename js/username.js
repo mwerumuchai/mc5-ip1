100c915e364188d8dacbca3734c6e09368d87e35
mwerumuchai <mwerumuchai@gmail.com>
2017-09-08 19:07:55 +0300

    add folder

--- js/username.js
+++ js/username.js
@@ -6,16 +6,22 @@ exports.Repo = function() {
 
 exports.Repo.prototype.getRepos = function(userName,public_repos,avatar_url) {
   $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response) {
+<<<<<<< HEAD
     $('.showUserName').append('<h4>Username:' + response.login + '</h4>');
     $('.NumberOfRepos').append('<h4>No. of respositories:' + response.public_repos + '</h4>');
     $('.Img').append("<img src=" + response.avatar_url + ">");
+=======
+    $('.userName').append('<h4>Username:' + response.login + '</h4>');
+    $('.NumberOfRepos').append('<h4>No of respositories:' + response.public_repos + '</h4>');
+    $('.showImg').append("<img src=" + response.avatar_url + ">");
+>>>>>>> parent of a10a996... add css files
   }).fail(function(error) {
     console.log(error.responseJSON.message);
   });
 
 $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
     for(var i = 0; i < response.length; i++){
-      $('.Repos').append('<h4>' + 'Repo name: ' + '<h5>' + response[i].name + '</h5><p>' + response[i].description + ' <a href="' + response[i].html_url + '"><br> <h5> Link </h5> </a></p>');
+      $('.Repos').append('<h3>' + 'Repo name: ' + '<h4>' + response[i].name + '</h4><p>' + response[i].description + ' <a href="' + response[i].html_url + '"><br> <h4> Link </h4> </a></p>');
     }
     $('.Repos').append('</ol>');
   }).fail(function(error) {
