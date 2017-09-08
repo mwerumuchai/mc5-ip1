47fde2af353e9c466628172e8cbba0d9553cb4fc
mwerumuchai <mwerumuchai@gmail.com>
2017-09-08 22:10:09 +0300

    add .env file

--- js/username-interface.js
+++ js/username-interface.js
@@ -4,14 +4,13 @@ var Repo = require ('./../js/username.js').Repo;
 $(document).ready(function() {
   var newUser = new Repo();
   $("#formId").submit(function(event) {
-<<<<<<< HEAD
+
     $('.showUserName').empty();
     $('.Img').empty();
     $('.NumberOfRepos').empty();
     $('.Repos').empty();
     $('.newUser').empty();
-=======
->>>>>>> parent of a10a996... add css files
+    
     event.preventDefault();
     var userName = $('#userInput').val();
     $('#userInput').val('');
--- js/username.js
+++ js/username.js
@@ -6,15 +6,11 @@ exports.Repo = function() {
 
 exports.Repo.prototype.getRepos = function(userName,public_repos,avatar_url) {
   $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response) {
-<<<<<<< HEAD
+    
     $('.showUserName').append('<h4>Username:' + response.login + '</h4>');
     $('.NumberOfRepos').append('<h4>No. of respositories:' + response.public_repos + '</h4>');
     $('.Img').append("<img src=" + response.avatar_url + ">");
-=======
-    $('.userName').append('<h4>Username:' + response.login + '</h4>');
-    $('.NumberOfRepos').append('<h4>No of respositories:' + response.public_repos + '</h4>');
-    $('.showImg').append("<img src=" + response.avatar_url + ">");
->>>>>>> parent of a10a996... add css files
+
   }).fail(function(error) {
     console.log(error.responseJSON.message);
   });
