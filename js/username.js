1fc2387cbbd9ce6e8c5887a130bb02b1e413699e
mwerumuchai <mwerumuchai@gmail.com>
2017-09-09 12:21:00 +0300

    modify files

--- js/username.js
+++ js/username.js
@@ -7,8 +7,8 @@ exports.Repo = function() {
 exports.Repo.prototype.getRepos = function(userName,public_repos,avatar_url) {
   $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response) {
 
-    $('.showUserName').append('<h4><strong>Username:' + response.login + '</strong></h4>');
-    $('.NumberOfRepos').append('<h4><strong>No. of respositories:' + response.public_repos + '</sstrong></h4>');
+    $('.showUserName').append('<h4><strong><i>Username: ' + response.login + '</i></strong></h4>');
+    $('.NumberOfRepos').append('<h4><strong><i>No. of respositories: ' + response.public_repos + '</i></strong></h4>');
     $('.Img').append("<img src=" + response.avatar_url + ">");
 
   }).fail(function(error) {
